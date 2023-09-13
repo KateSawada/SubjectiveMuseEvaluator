1c06588dded6af0bef6d9091e1ca8e258255b462
KateSawada <sawapy630@gmail.com>
2023-09-13 19:27:49 +0900

    implements empty bars

--- units.py
+++ units.py
@@ -50,4 +50,47 @@ class EvaluateUnit(ABC):
         """
         return
 
+
+class EmptyBars(EvaluateUnit):
+    def __init__(
+        self,
+        n_samples: int,
+        n_pianoroll_per_samples: int,
+        reshaper: Callable[[np.ndarray], np.ndarray],
+        track_names: List[str],
+    ):
+        """Empty bars ratio
+        Args:
+            n_samples (int): count of total samples
+            n_pianoroll_per_samples (int): pianoroll per samples
+            reshaper (Callable[[np.ndarray], np.ndarray]): reshape function.
+                it convert pianoroll shape into (song, track, timestep, pitch)
+            track_names (List[str]): list of track name
+        """
+        self.results = np.zeros((n_samples, len(track_names)))
+        self.n_pianoroll_per_samples = n_pianoroll_per_samples
+        self.reshaper =reshaper
+        self.track_names = track_names
+
+    def __call__(self, idx: int, pianoroll: np.ndarray) -> None:
+        """calculate empty bars ratio
+
+        Args:
+            idx (int): index of the sample
+            pianoroll (np.ndarray): pianoroll of n songs.
+        """
+        pianoroll = self.reshaper(pianoroll)
+
+        result = np.all(pianoroll == 0, axis=(2, 3)).astype(np.float32)
+        self.results[
+            idx * self.n_pianoroll_per_samples:
+                (idx + 1) * self.n_pianoroll_per_samples] = result
+
+    def to_dict(self):
+        results = {}
+        for i in range(len(self.track_names)):
+            results[self.track_names[i]] = np.average(self.results[:, i]).item()
+        return {"empty_bars": results}
+
+
 # TODO: implement each methods
