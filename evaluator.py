d0c5703ba042bd738294e84fcac23b951dc3d058
KateSawada <sawapy630@gmail.com>
2023-09-13 18:14:52 +0900

    base修正

--- evaluator.py
+++ evaluator.py
@@ -1,11 +1,18 @@
 from typing import List
 
+import numpy as np
+
 from .units import EvaluateUnit
 
 
 class Evaluator:
-    def __init__(self, methods: List[EvaluateUnit]):
+    def __init__(
+            self,
+            methods: List[EvaluateUnit],
+            samples: List[np.ndarray],
+    ):
         self.methods = methods
+        self.samples = samples
 
     def run(self, output_file: str):
         """run evaluations
--- units.py
+++ units.py
@@ -5,32 +5,48 @@ from abc import (
     ABC,
 )
 from typing import (
-    Union,
+    List,
     Dict,
     Any,
+    Callable,
 )
 
 import numpy as np
 
 
 class EvaluateUnit(ABC):
-    @property
     @abstractmethod
-    def return_type(self):
-        return int  # int, float, or np.ndarray
+    def __init__(
+        self,
+        n_samples: int,
+        n_pianoroll_per_samples: int,
+        reshaper: Callable[[np.ndarray], np.ndarray],
+        track_names: List[str],
+    ):
+        """Abstract class for Evaluation Unit
+
+        Args:
+            n_samples (int): count of total samples
+            n_pianoroll_per_samples (int): pianoroll per samples
+            reshaper (Callable[[np.ndarray], np.ndarray]): reshape function.
+                it convert pianoroll shape into (song, track, timestep, pitch)
+            track_names (List[str]): list of track name
+        """
+        self.results = np.zeros(n_samples)
+        self.n_pianoroll_per_samples = n_pianoroll_per_samples
+        self.reshaper =reshaper
+        self.track_names = track_names
 
     @abstractmethod
     def to_dict(self) -> Dict[str, Any]:
         return {"unit_name": 0}
 
     @abstractmethod
-    def __call__(self, pianoroll: np.ndarray) -> Union[int, float, np.ndarray]:
+    def __call__(self, idx: int, pianoroll: np.ndarray):
         """run evaluation
         Args:
-            pianoroll (np.ndarray): pianoroll of one song
-
-        Returns:
-            Union[int, float, np.ndarray]: result
+            idx (int): index of the sample
+            pianoroll (np.ndarray): pianoroll of n songs.
         """
         return
 
